c95f8e1d3426c11c385c67de8e2fbe810326bb9c
eongyu <lhi012633@gmail.com>
2024-05-13 14:43:02 +0900

    assembler

--- project1/assembler/assembler.c
+++ project1/assembler/assembler.c
@@ -6,15 +6,24 @@
 
 #define MAXLINELENGTH 1000
 
-int readAndParse(FILE *, char *, char *, char *, char *, char *);
+int readAndParse(FILE *, char *, char *, char *, char *, char *); // 라벨, opcode, regA, regB, offset
 int isNumber(char *);
+int reg[8];
+int adrs[50];
+
+typedef struct{
+    char lab[7]; // 라벨 이름.
+    int address; // 라벨 주소. (프로그램 카운트)
+} LabelAddressMap;
+
+int findLabel(char *label, LabelAddressMap *labelMap, int labelCount);
 
 int main(int argc, char *argv[]) 
 {
 	char *inFileString, *outFileString;
 	FILE *inFilePtr, *outFilePtr;
 	char label[MAXLINELENGTH], opcode[MAXLINELENGTH], arg0[MAXLINELENGTH], 
-			 arg1[MAXLINELENGTH], arg2[MAXLINELENGTH];
+			 arg1[MAXLINELENGTH], arg2[MAXLINELENGTH]; // 배열을 하나씩 만듦.
 
 	if (argc != 3) {
 		printf("error: usage: %s <assembly-code-file> <machine-code-file>\n",
@@ -25,12 +34,12 @@ int main(int argc, char *argv[])
 	inFileString = argv[1];
 	outFileString = argv[2];
 
-	inFilePtr = fopen(inFileString, "r");
+	inFilePtr = fopen(inFileString, "r"); // 파일 읽기. as file.
 	if (inFilePtr == NULL) {
 		printf("error in opening %s\n", inFileString);
 		exit(1);
 	}
-	outFilePtr = fopen(outFileString, "w");
+	outFilePtr = fopen(outFileString, "w"); // 파일 쓰기. mc file.
 	if (outFilePtr == NULL) {
 		printf("error in opening %s\n", outFileString);
 		exit(1);
@@ -38,18 +47,97 @@ int main(int argc, char *argv[])
 
 	/* here is an example for how to use readAndParse to read a line from
 		 inFilePtr */
-	if (!readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)) {
+	//if (!readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)) {
 		/* reached end of file */
-	}
+	//}
 
 	/* TODO: Phase-1 label calculation */
+    int address = 0; // 프로그램 카운트.
+    LabelAddressMap labelMap[100]; // 라벨 100개 저장 가능.
+    int labelCount = 0;
+    while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)) // 파일의 끝까지 반복.
+    {
+        if(label[0] != '\0'){
+            if(findLabel(label, labelMap, labelCount) == -1){ // 라벨이 등록되어있지 않은 경우. 등록.
+                strcpy(labelMap[labelCount].lab, label);
+                labelMap[labelCount].address = address;
+                labelCount++;
+            }
+        }
+        if(opcode[0] == '.'){
+            if(isNumber(arg0)){
+                adrs[address] = atoi(arg0);
+            }
+            else{
+                int idx = findLabel(arg0, labelMap, labelCount);
+                adrs[address] = labelMap[idx].address;
+            }
+        }
+        address++;
+    }
+    for (int i = 0; i<labelCount; i++){
+        printf("%s: %d\n", labelMap[i].lab, labelMap[i].address);
+    }
 
 	/* this is how to rewind the file ptr so that you start reading from the
 		 beginning of the file */
 	rewind(inFilePtr);
 
 	/* TODO: Phase-2 generate machine codes to outfile */
+    int pc = 0;
+    while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2)){
+        int line[32];
+        int a;
+        if (!strcmp(opcode, "add")){
+            a = ((atoi(arg0) & 7)<<19)|((atoi(arg1) & 7)<<16)|(atoi(arg2) & 7);
 
+        }
+        else if (!strcmp(opcode, "nor")){
+            a = (1<<22)|((atoi(arg0) & 7)<<19)|((atoi(arg1) & 7)<<16)|(atoi(arg2) & 7);
+        }
+        else if (!strcmp(opcode, "lw")){
+            if(atoi(arg0) == 0){ // arg0 이 0일 경우, symbolic address.
+                int idx = findLabel(arg2, labelMap, labelCount);
+                a = (labelMap[idx].address & 65535)|((atoi(arg0)&7)<<19)|((atoi(arg1)&7)<<16)|(1<<23);
+            }
+            else{ // 아닌 경우는 numeric address.
+                a = ((atoi(arg0)&7)<<19)|((atoi(arg1)&7)<<16)|(1<<23)|(atoi(arg2)&7);
+            }
+        }
+        else if (!strcmp(opcode, "sw")){
+            if(atoi(arg0) == 0){ // arg0 이 0일 경우, symbolic address.
+                int idx = findLabel(arg2, labelMap, labelCount);
+                a = (labelMap[idx].address & 65535)|((atoi(arg0) & 7)<<19)|((atoi(arg1) & 7)<<16)|(3<<22);
+            }
+            else{ // 아닌 경우는 numeric address.
+                adrs[reg[atoi(arg0)]+atoi(arg2)] = reg[atoi(arg1)];
+            }
+        }
+        else if (!strcmp(opcode, "beq")){
+            if(isNumber(arg2)){
+                a = (1<<24)|((atoi(arg0) & 7)<<19)|((atoi(arg1) & 7)<<16)|(atoi(arg2) & 7);
+            }
+            else{
+                int idx = findLabel(arg2, labelMap, labelCount);
+                int offset = labelMap[idx].address - pc - 1;
+                a = (offset & 65535)|((atoi(arg0)&7)<<19)|((atoi(arg1)&7)<<16)|(1<<24);
+            }
+        }
+        else if(!strcmp(opcode, "jalr")){
+            a = (5<<22)|(atoi(arg0)<<19)|(atoi(arg1)<<16);
+        }
+        else if(!strcmp(opcode, "halt")){
+            a = (6<<22);
+        }
+        else if(!strcmp(opcode, "noop")){
+            a = (7<<22);
+        }
+        else if(!strcmp(opcode, ".fill")){
+            a = adrs[pc];
+        }
+        fprintf(outFilePtr, "(address %d): %d (hex 0x%x)\n", pc, a, a);
+        pc++;
+    }
 	/* after doing a readAndParse, you may want to do the following to test the
 		 opcode */
 	if (!strcmp(opcode, "add")) {
@@ -121,3 +209,11 @@ int isNumber(char *string)
 	return( (sscanf(string, "%d", &i)) == 1);
 }
 
+int findLabel(char *label, LabelAddressMap *labelMap, int labelCount){
+    for(int i = 0; i<labelCount; i++){
+        if(strcmp(label, labelMap[i].lab) == 0) {
+            return i; // 이미 있으면 그 idx 리턴.
+        }
+    }
+    return -1; // 만약 새로운 라벨이면 -1 리턴.
+}
